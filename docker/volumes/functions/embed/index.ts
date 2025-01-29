d768e708225368d4379a264b8a8e9e21e855f2fd
Lorenzo Albertin <lorenzo.albertin@studenti.unipd.it>
2025-01-29 13:12:20 +0100

    aggiornata funzione embed: ora openai

--- docker/volumes/functions/embed/index.ts
+++ docker/volumes/functions/embed/index.ts
@@ -4,52 +4,61 @@
 
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { createClient } from 'jsr:@supabase/supabase-js';
-import { Embedding } from 'jsr:@epi/ollama';
+import { OpenAI } from "npm:openai";
 
 
 Deno.serve(async (req) => {
-    try {
-      const supabaseUrl = Deno.env.get('SUPABASE_URL');
-      const supabaseAnonKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY');
-    
-      if (!supabaseUrl || !supabaseAnonKey) {
-          throw new Error('Missing environment variables SUPABASE_URL or ANON_KEY');
-      }
 
-      const authorization = req.headers.get('Authorization');
+  try {
+    const supabaseUrl = Deno.env.get('SUPABASE_URL');
+    const supabaseAnonKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY');
 
-      if (!authorization) {
-        return new Response(
-          JSON.stringify({ error: `No authorization header passed` }),
-          {
-            status: 500,
-            headers: { 'Content-Type': 'application/json' },
-          }
-        );
-      }
-      const supabase = createClient(supabaseUrl, supabaseAnonKey);
-        // Extract input string from JSON body
-      const { content, manuale, n_chunk } = await req.json();
-      // Generate the embedding from the user input
-      const embed = await Embedding(content, 'nomic-embed-text', 'http://ollama:11434');
-
-      const { error } = await supabase
-        .from('manuale_sezione')
-        .update({embedding: embed })
-        .match({manuale: manuale, nchunk:n_chunk})
-      // Return the embedding
-      if (error) {
-        throw new Error(`Error inserting embedding: ${error.message}`);
-      }
-
-      return new Response(
-        JSON.stringify({ 'result':'ok' }),
-        { headers: { 'Content-Type': 'application/json' } }
-      );
-    } catch (error) {
-        console.error('Error handling request:', error);
-        return new Response('Internal Server Error', { status: 500 });
+    if (!supabaseUrl || !supabaseAnonKey) {
+      throw new Error('Missing environment variables SUPABASE_URL or ANON_KEY');
     }
+
+    const authorization = req.headers.get('Authorization');
+
+    if (!authorization) {
+      return new Response(
+        JSON.stringify({ error: `No authorization header passed` }),
+        {
+          status: 500,
+          headers: { 'Content-Type': 'application/json' },
+        }
+      );
+    }
+    const supabase = createClient(supabaseUrl, supabaseAnonKey);
+    // Extract input string from JSON body
+    const { content, manuale, n_chunk } = await req.json();
+    // Generate the embedding from the user input
+    const llmIstance = new OpenAI({
+      baseURL : 'http://ollama:11434/v1',
+      apiKey: 'ollama' 
+    });
+
+    const embed = await llmIstance.embeddings.create({
+      model: 'nomic-embed-text',
+      input: 'search_document: '+content
+    });
+
+    const { error } = await supabase
+      .from('manuale_sezione')
+      .update({ embedding: embed.data[0].embedding })
+      .match({ manuale: manuale, nchunk: n_chunk })
+    // Return the embedding
+    if (error) {
+      throw new Error(`Error inserting embedding: ${error.message}`);
+    }
+
+    return new Response(
+      JSON.stringify({ 'result': 'ok' }),
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  } catch (error) {
+    console.error('Error handling request:', error);
+    return new Response('Internal Server Error', { status: 500 });
+  }
 })
 
 // To invoke:
