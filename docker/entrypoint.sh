392af3d07c31c4ef3c2634d3a707af9b167fe9d8
Lorenzo Albertin <lorenzo.albertin@studenti.unipd.it>
2025-01-29 13:08:33 +0100

    aggiornato docker-compose.yml; aggiunto docker-compose.app.yml per container webapp; aggiornato entrypoint per scaricare modelli

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,6 +12,11 @@ echo "🔴 Retrieve nomic-embed-text model..."
 ollama pull nomic-embed-text
 echo "🟢 Done!"
 
+echo "🔴 Retrieve llama3.2:3b model..."
+ollama pull deepseek-r1:14b
+echo "🟢 Done!"
+
+
 # Add model for answers
 
 # Wait for Ollama process to finish.
